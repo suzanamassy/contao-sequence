2fe5ac220aeea100eba304c553ffc2967259a939
merlin <github@jedo-style.de>
2014-08-10 08:16:01 +0200

    start init

---
+++ Contao3/system/module/jedo-extensions/jedo-sequence-slider/elements/2.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   s
+ * @author    jedoStyle
+ * @license   s
+ * @copyright s
+ */
+
+
+/**
+ * Namespace
+ */
+namespace s;
+
+
+/**
+ * Class 2
+ *
+ * @copyright  s
+ * @author     jedoStyle
+ * @package    Devtools
+ */
+class 2 extends \Module
+{
+
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = '';
+
+
+	/**
+	 * Generate the module
+	 */
+	protected function compile()
+	{
+
+	}
+}
